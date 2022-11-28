7180ad8cc345c48f75360e3564a6e9f949cad372
mulukimwangeka <mwangekaruth@gmail.com>
2022-11-28 15:36:10 +0300

    Routes created

---
+++ app/models/plant.rb
@@ -0,0 +1,25 @@
+class Plant < ApplicationRecord
+    def index
+        plants=Plant.all
+        render json: plants
+
+    end
+
+    def show
+        plant=Plant.find(params[:id])
+        if plant
+        render json: plant
+        else
+            render json: {error: "Plant not found"}, status: 404
+        end
+    end
+
+    def create
+        plant=Plant.create(plant_params)
+        render json: plant,status: :created
+    end
+    private
+    def plant_params
+        params.permit(:name,:image,:price)
+    end
+end
